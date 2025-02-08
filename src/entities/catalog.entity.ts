9a727c9b187660df9ba77b1c79f43830ac747507
boburDev <65768571+boburDev@users.noreply.github.com>
2025-02-08 09:50:45 +0500

    added catalog brands products

---
+++ src/entities/catalog.entity.ts
@@ -0,0 +1,69 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, DeleteDateColumn, ManyToOne, JoinColumn, OneToMany, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Catalog {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true, length: 255 })
+    title: string;
+
+    @OneToMany(() => Subcatalog, subcatalog => subcatalog.catalog)
+    subcatalogs: Subcatalog[];
+
+    @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
+
+@Entity()
+export class Subcatalog {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true, length: 255 })
+    title: string;
+
+    @Column()
+    catalogId: string;
+
+    @ManyToOne(() => Catalog, catalog => catalog.subcatalogs, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'catalogId' })
+    catalog: Catalog;
+
+    @OneToMany(() => Category, category => category.subCatalog)
+    categories: Category[];
+
+    @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
+
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true, length: 255 })
+    title: string;
+
+    @Column({ length: 500 })
+    path: string;
+
+    @Column()
+    subCatalogId: string;
+
+    @ManyToOne(() => Subcatalog, subcatalog => subcatalog.categories, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'subCatalogId' })
+    subCatalog: Subcatalog;
+
+    @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
