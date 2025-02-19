6d2480fe37ceb901cb34732d15772d1a44630fc7
Boburmirzo Negmatov <65768571+boburDev@users.noreply.github.com>
2025-02-19 19:33:07 +0500

    Update user.entity.ts

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -16,7 +16,7 @@ export class User {
     @Column()
     password: string;
 
-    @Column()
+    @Column({ nullable: false})
     phone: string;
 
     @OneToMany(() => ProductQuestion, (question) => question.user)
