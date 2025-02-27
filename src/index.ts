ac4bf9cdb8449fe265143a5ab9152ca7fac3daec
boburDev <65768571+boburDev@users.noreply.github.com>
2025-02-27 11:37:05 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,8 @@ AppDataSource.initialize()
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.static(path.join("public")));
 
 app.use(
