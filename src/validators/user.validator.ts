ced4d52160e5e40460529a25c2ae541818733628
boburDev <65768571+boburDev@users.noreply.github.com>
2025-03-04 12:13:59 +0500

    Update user.validator.ts

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,9 +1,8 @@
 import Joi from "joi";
 
 export const userSchemaValidator = Joi.object({
-    name: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    optCode: Joi.string().required(),
 });
 
 
