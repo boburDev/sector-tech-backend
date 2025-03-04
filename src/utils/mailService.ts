47a1add613c97d6ca54321c10f82b52e964d3edf
boburDev <65768571+boburDev@users.noreply.github.com>
2025-03-04 11:51:26 +0500

    Update mailService.ts

--- src/utils/mailService.ts
+++ src/utils/mailService.ts
@@ -3,6 +3,9 @@ import nodemailer from 'nodemailer';
 export const mailService = async (email: string, otp: string) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
